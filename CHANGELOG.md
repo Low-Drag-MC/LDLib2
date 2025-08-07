e50f94311189eeaff38452348ab36833f3ab6f7d
KilaBash <yefancy@foxmail.com>
2025-08-07 10:00:31 +0900

    fix mod loading check

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,4 @@
 # ChangeLog
 
-## v2.0.2
-* Move file assets from the `assets` to the `ldlib2` folder
-* Fixed cross-OS platform file separator char
+## v2.0.3
+* Fixed Mixins check
--- src/main/java/com/lowdragmc/lowdraglib2/core/mixins/MixinPluginShared.java
+++ src/main/java/com/lowdragmc/lowdraglib2/core/mixins/MixinPluginShared.java
@@ -1,26 +1,26 @@
 package com.lowdragmc.lowdraglib2.core.mixins;
 
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.LoadingModList;
+
 public interface MixinPluginShared {
 
-	static boolean isClassFound(String className) {
-		try {
-			Class.forName(className, false, Thread.currentThread().getContextClassLoader());
-			return true;
-		} catch (ClassNotFoundException e) {
-			return false;
+	static boolean isModLoaded(String modId) {
+		if (ModList.get() == null) {
+			return LoadingModList.get().getModFileById(modId) != null;
 		}
+		return ModList.get().isLoaded(modId);
 	}
 
-	boolean IS_OPT_LOAD = isClassFound("optifine.OptiFineTranformationService");
-
-	boolean IS_SODIUM_LOAD = isClassFound("net.caffeinemc.mods.sodium.mixin.SodiumMixinPlugin");
-	boolean IS_JEI_LOAD = isClassFound("mezz.jei.api.JeiPlugin");
-	boolean IS_REI_LOAD = isClassFound("me.shedaniel.rei.api.common.plugins.REIPlugin");
-	boolean IS_MEI_LOAD = isClassFound("dev.emi.emi.api.EmiPlugin");
+	boolean IS_OPT_LOAD = isModLoaded("optifine");
+	boolean IS_SODIUM_LOAD = isModLoaded("sodium");
+	boolean IS_JEI_LOAD = isModLoaded("jei");
+	boolean IS_REI_LOAD = isModLoaded("rei");
+	boolean IS_MEI_LOAD = isModLoaded("emi");
 	boolean IS_EMI_LOADED = IS_MEI_LOAD;
 	boolean IS_RUBIDIUM_LOAD = IS_SODIUM_LOAD;
-	boolean IS_IRIS_LOAD = isClassFound("net.irisshaders.iris.Iris");
-	boolean IS_OCULUS_LOAD = IS_IRIS_LOAD;
-	boolean IS_KJS_LOAD = isClassFound("dev.latvian.mods.kubejs.KubeJS");
+	boolean IS_IRIS_LOAD = isModLoaded("iris");
+	boolean IS_OCULUS_LOAD = IS_IRIS_LOAD || isModLoaded("oculus");
+	boolean IS_KJS_LOAD = isModLoaded("kubejs");
 
 }
