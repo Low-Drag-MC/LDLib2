006369d5f50e67e40f738dbfdad6554fdaab75c2
KilaBash <yefancy@foxmail.com>
2026-01-18 20:10:19 +0900

    improve doc

--- src/main/java/com/lowdragmc/lowdraglib2/syncdata/annotation/ConditionalSynced.java
+++ src/main/java/com/lowdragmc/lowdraglib2/syncdata/annotation/ConditionalSynced.java
@@ -10,17 +10,16 @@ import java.lang.annotation.Target;
 public @interface ConditionalSynced {
     /**
      * Specifies the method name that will be used to determine the conditional sync behavior
-     * for the annotated field. This method should exist in the containing class and return
-     * a {@code String} value that represents this conditional behavior.
+     * for the annotated field.
+     * This value is used to map the annotated field to control whether it syncs if changes.
      *
      * <pre>{@code
      * @Configurable
      * @ConditionalSynced(methodName = "shouldSync")
      * int intField = 10;
      *
-     * @SkipPersistedValue(field = "intField")
      * public boolean shouldSync(int value) {
-     *     return value == 10;
+     *     return value > 0;
      * }}</pre>
      *
      * @return the name of the method used to evaluate the conditional sync operation.
