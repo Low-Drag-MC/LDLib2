77c43fcaa735c4e051e6d595646794ffd592a42f
KilaBash <yefancy@foxmail.com>
2026-05-26 00:45:46 +0900

    Fixed ModularHudLayer screen size to respect scale

--- src/main/java/com/lowdragmc/lowdraglib2/gui/hud/ModularHudLayer.java
+++ src/main/java/com/lowdragmc/lowdraglib2/gui/hud/ModularHudLayer.java
@@ -25,8 +25,8 @@ public interface ModularHudLayer extends GuiLayer {
      * @return a {@link Size} instance representing the width and height of the game screen.
      */
     default Size getScreenSize() {
-        return Size.of(Minecraft.getInstance().getWindow().getScreenWidth(),
-                Minecraft.getInstance().getWindow().getScreenHeight());
+        return Size.of(Minecraft.getInstance().getWindow().getGuiScaledWidth(),
+                Minecraft.getInstance().getWindow().getGuiScaledHeight());
     }
 
     /**
